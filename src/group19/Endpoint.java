f668c94462afc183143474ca4db485019021f41d
wasartin@iastate.edu <wasartin@iastate.edu>
2019-03-14 09:49:27 -0500

    importing skeleton of project

---
+++ src/group19/Endpoint.java
@@ -0,0 +1,13 @@
+package group19;
+
+public class Endpoint {
+
+	/**
+	 *  Returns the endpoint value. For example if the Endpoint object
+	 *  represents the left endpoint of the interval [1, 3], this would return 1.
+	 * @return
+	 */
+	public int getValue() {
+		return -1;
+	}
+}
---
+++ src/group19/RBTree.java
@@ -0,0 +1,44 @@
+package group19;
+
+public class RBTree {
+
+	public RBTree() {
+		
+	}
+	
+	/**
+	 * Returns the root node.
+	 * @return
+	 */
+	public Node getRoot() {
+		//TODO
+		return null;
+	}
+	
+	/**Returns the nil node.
+	 * Note. A red-black tree T must contain exactly one instance of T.nil.
+	 * @return
+	 */
+	public Node getNILNode() {
+		//TODO
+		return null;
+	}
+	
+	/**
+	 * Returns the number of internal nodes in the tree.
+	 * @return
+	 */
+	public int getSize() {
+		//TODO
+		return -1;
+	}
+	
+	/**
+	 *  Returns the height of the tree
+	 * @return
+	 */
+	public int getHeight() {
+		//TODO
+		return -1;
+	}
+}
