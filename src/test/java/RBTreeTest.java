3a16e6e818ed425b50d333351ee9185c60396d18
wasartin@iastate.edu <wasartin@iastate.edu>
2019-03-14 15:21:39 -0500

    Just made some JUnit test classes, anything made is just a dummy test

---
+++ src/test/java/RBTreeTest.java
@@ -0,0 +1,56 @@
+package test.java;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class RBTreeTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	
+	/**
+	 * Every Node is either red or black
+	 */
+	@Test
+	public void RBTreePropertyOne_Success() {
+		
+	}
+	
+	/**
+	 * Root is Black
+	 */
+	@Test
+	public void RBTreePropertyTwo_Success() {
+	}
+	
+	/**
+	 * Every leaf(nil) is black
+	 * Note: we are doing T.il in our RBTree
+	 */
+	@Test
+	public void RBTreePropertyThree_Success() {
+		
+	}
+	
+	/**
+	 * If a node is red then both of its children are black
+	 */
+	@Test
+	public void RBTreePropertyFour_Success() {
+		
+	}
+	
+	/**
+	 * For each node, all simple paths from the node to the 
+	 * descendant leaves contain the same number of black nodes
+	 */
+	@Test
+	public void RBTreePropertyFive_Success() {
+		
+		
+	}
+
+}
