e19b89da869108b3b4c766c3989e6bc401925ccf
wasartin@iastate.edu <wasartin@iastate.edu>
2019-04-21 18:09:18 -0500

    Junk

--- src/Communication.java
+++ src/Communication.java
@@ -7,9 +7,29 @@ public class Communication {
         this.c2 = c2;
         this.timestamp = timestamp;
     }
-    
-    public int getTimestamp() {
-    	return this.timestamp;
-    }
 
+	public int getC1() {
+		return c1;
+	}
+
+	public void setC1(int c1) {
+		this.c1 = c1;
+	}
+
+	public int getC2() {
+		return c2;
+	}
+
+	public void setC2(int c2) {
+		this.c2 = c2;
+	}
+
+	public int getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(int timestamp) {
+		this.timestamp = timestamp;
+	}
+    
 }
